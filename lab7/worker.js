7f5cdf2162fa4f72eebee1176198d873bbfd9830
Luvkitri <janjedrzejewski@gmail.com>
2021-05-05 11:26:13 +0200

    lab 6 cleanup and added some logic to worker

--- lab7/worker.js
+++ lab7/worker.js
@@ -1,3 +1,24 @@
+function swapCase(word) {
+  let letters = word.split('');
+  let newWord = [];
+
+  for (letter of letters) {
+    if (letter == letter.toUpperCase()) {
+      newWord.push(letter.toLowerCase());
+    }
+    
+    if (letter == letter.toLowerCase()) {
+      newWord.push(letter.toUpperCase());
+    }
+  }
+
+  return newWord.toString();
+}
+
 onmessage = function (e) {
-    console.log(JSON.parse(e.data));
+  let parseObject = JSON.parse(e.data);
+
+  Object.keys(parseObject).foreach(key => {
+    console.log(swapCase(parseObject[key]));
+  })
 };
